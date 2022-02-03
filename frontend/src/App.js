04630274f2a8aec22dc38d07ce57b9a8711db4ee
Kapil Chaudhary <chaudharikapil16@gmail.com>
2022-02-03 13:08:22 +0530

    frontend - install chakra UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Button } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className="bg-blue-300 p-4 text-5xl text-center">
       Let us connect
+      <Button colorScheme='blue'>Button</Button>
     </div>
   );
 }
