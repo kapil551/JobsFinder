cbee95116ca214e1cce6b6432db5ba929ddefe6c
Kapil Chaudhary <chaudharikapil16@gmail.com>
2022-02-03 12:41:07 +0530

    backend - creating node JS server and setting up express JS API

---
+++ README.md
@@ -0,0 +1,29 @@
+# Chat Fata Fat - Realtime Chat Application
+
+## Tech Stack - React.js, Express.js, Node.js, MongoDB, Socket.io
+
+Front End -> React.js
+Back End -> Express.js, Node.js
+Database -> MongoDB
+
+## Project Data Flow:
+    
+    -
+
+    - 
+
+    - 
+
+    -
+
+## Project Overview:
+
+    - 
+
+    -
+
+    - 
+
+    -
+
+## Project Screenshots:
---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require('dotenv');
+
+const { chats } = require("./data/data");
+
+const app = express();
+dotenv.config();
+
+// GET --> '/'
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+// GET --> '/api/chat'
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+// GET -->
+app.get("/api/chat/:id", (req, res) => {
+  //console.log(req.params.id);
+
+  const singleChat = chats.find((chat) => chat._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("server started...");
+});
