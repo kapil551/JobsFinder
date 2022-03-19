1ffbea11375f89c102da46651abf63d47e40f53d
Kapil Chaudhary <100148666+kapilcr7@users.noreply.github.com>
2022-03-19 23:12:17 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const colors = require('colors')
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
+const path = require("path");
 
 //middleware
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
@@ -27,6 +28,24 @@ app.use(express.json()); // to tell the server to accept JSON data from the fron
 app.use('/api/user', userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running successfully");
+  });
+}
+
+// --------------------------deployment------------------------------
 
 // Error handling middlewares
 app.use(notFound);
--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -15,7 +15,7 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:5000"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
+const ENDPOINT = "https://let-us-connect-chat-module.herokuapp.com/"; // "https://let-us-connect-chat-module.herokuapp.com/; -> After deployment
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
